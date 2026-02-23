d7a83fa4b3461a8d32d4cc70cda523991a7d5270
Fita04 <crackers0404@gmail.com>
2026-02-23 20:25:20 +0800

    Added a function to get the player's choice. Added a function that gets the player's move via a prompt. Assuming that they spelled the words right, any variation of them will be valid and will return the corresponding value. Ex:"Rock", "RocK", and "rock" will return the value "Rock"

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-// Create a function that returns the computer's choice of rock, paper, or scissors
-function getComputerChoice () {
+function getComputerChoice () { // Create a function that returns the computer's choice of rock, paper, or scissors
 
-    let computerChoice = Math.random() // Assign a numerical value for each possible move. Use Math.floor(Math.random()) for this
+    let computerChoice = Math.random(); // Assign a numerical value for each possible move. Use (Math.random()) for this
 
-    if (computerChoice < 0.34) { return("Rock")
+    if (computerChoice < 0.34) { return("Rock");
         
-    } else if (computerChoice > 0.34 && computerChoice < 0.64) { return("Paper")
-        
-    } else {return ("Scissors")
+    } else if (computerChoice > 0.34 && computerChoice < 0.64) { return("Paper");
+
+    } else { return ("Scissors");
 
     }
+}
+
+function getHumanChoice () {
+    let humanChoice = prompt("What's your move?");
+
+    if (humanChoice.toLowerCase() === "rock") { return("Rock");
+        
+    } else if (humanChoice.toLowerCase() === "paper") { return("Paper")
+        
+    } else if (humanChoice.toLowerCase() === "scissors") { return("Scissors") 
+
+    }
+
 }
 
