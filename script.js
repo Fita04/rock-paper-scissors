56f9599319613e153ae58b059795f4cc837a43fc
Fita04 <crackers0404@gmail.com>
2026-02-23 22:41:50 +0800

    Added the rest of the game logic in playRound Added the rest of the game logic in the playRound function and added each result that wasn't covered by the draw statement. Added the constants humanSelection and computerSelection which uses the getChoice functions to get a value.

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function getComputerChoice () { // Create a function that returns the computer's
 
     let computerChoice = Math.random(); // Assign a numerical value for each possible move. Use (Math.random()) for this
 
-    if (computerChoice < 0.34) { return("Rock");
+    if (computerChoice < 0.34) { return("rock");
         
-    } else if (computerChoice > 0.34 && computerChoice < 0.67) { return("Paper");
+    } else if (computerChoice > 0.34 && computerChoice < 0.67) { return("paper");
 
-    } else { return ("Scissors");
+    } else { return ("scissors");
 
     }
 }
@@ -14,25 +14,33 @@ function getComputerChoice () { // Create a function that returns the computer's
 function getHumanChoice () { // Create a function that returns the player's choice by using a prompt
     let humanChoice = prompt("What's your move?");
 
-    if (humanChoice.toLowerCase() === "rock") { return("Rock"); // the .toLowerCase method was added so that any variation of the moves are valid, provided they're spelled correctly
+    if (humanChoice.toLowerCase() === "rock") { return("rock"); // the .toLowerCase method was added so that any variation of the moves are valid, provided they're spelled correctly
         
-    } else if (humanChoice.toLowerCase() === "paper") { return("Paper")
+    } else if (humanChoice.toLowerCase() === "paper") { return("paper")
         
-    } else if (humanChoice.toLowerCase() === "scissors") { return("Scissors") 
+    } else if (humanChoice.toLowerCase() === "scissors") { return("scissors") 
 
     }
 
 }
+    function playRound (computerChoice, humanChoice) {
+    if (humanChoice === computerChoice) { return("Draw!");
+        
+    } else if (humanChoice === "scissors" && computerChoice === "rock" ) { console.log("Computer wins!")
 
-function playRound (humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) { return("Draw!")
+    } else if (humanChoice === "scissors" && computerChoice === "paper" ) { console.log("Player wins!")  
+          
+    } else if (humanChoice === "rock" && computerChoice === "paper") { console.log("Computer wins!") 
+    
+    } else if (humanChoice === "rock" && computerChoice === "scissors") { console.log ("Player wins!")
         
-    } else if () {
-        
+    } else if (humanChoice === "paper" && computerChoice === "scissors") { console.log ("Computer wins!")
+
+    } else if (humanChoice === "paper" && computerChoice === "rock") {console.log("Computer wins!")
+    
     }
-}
 
-let humanScore = 0;
-let computerScore = 0;
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+    let humanScore = 0;
+    let computerScore = 0;
+   
+    }
