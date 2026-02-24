6277ffafefcaf8cb93812accdd9a800812686915
Fita04 <crackers0404@gmail.com>
2026-02-24 09:30:30 +0800

    Added logic for increasing scores Added the logic for increasing scores by incrementing the winner's score (Eg. humanScore++) and returning that incremented value via return. It is imperative that the logic for increasing scores is put AFTER the console.log message. I tested it and putting the logic first prevents every console.log message from showing up after a round. The only exception to this is if a round ends in a draw, which doesn't need the logic because a draw won't affect either player's scores.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function getComputerChoice () { // Create a function that returns the computer's choice of rock, paper, or scissors
 
-    let computerChoice = Math.random(); // Assign a numerical value for each possible move. Use (Math.random()) for this
+    let computerChoice = Math.random(); // Assign a numerical value for each possible move. Use Math.random() for this
 
     if (computerChoice < 0.34) { return("rock");
         
@@ -25,25 +25,31 @@ function getHumanChoice () { // Create a function that returns the player's choi
 }
 
 function playRound (humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) { console.log("Draw!");
+        if (humanChoice === computerChoice) { console.log("Draw!");
+            
+        } else if (humanChoice === "scissors" && computerChoice === "rock" ) { 
+           console.log("Rock beats scissors! Computer wins!"); computerScore++; return(computerScore);  // computerScore++ increments the value of the variable by 1. Return sends it to the global scope so that it's value can be increased
+
+        } else if (humanChoice === "scissors" && computerChoice === "paper" ) { 
+            console.log("Scissors beats paper! Player wins!"); humanScore++; return(humanScore);  
+            
+        } else if (humanChoice === "rock" && computerChoice === "paper") { 
+            console.log("Paper beats rock! Computer wins!"); computerScore++; return(computerScore);  
         
-    } else if (humanChoice === "scissors" && computerChoice === "rock" ) { console.log("Rock beats scissors! Computer wins!")
+        } else if (humanChoice === "rock" && computerChoice === "scissors") { 
+            console.log ("Rock beats scissors! Player wins!"); humanScore++; return(humanScore);
+            
+        } else if (humanChoice === "paper" && computerChoice === "scissors") { 
+            console.log ("Scissors beats paper! Computer wins!"); computerScore++; return(computerScore); 
 
-    } else if (humanChoice === "scissors" && computerChoice === "paper" ) { console.log("Scissors beats paper! Player wins!")  
-          
-    } else if (humanChoice === "rock" && computerChoice === "paper") { console.log("Paper beats rock! Computer wins!") 
-    
-    } else if (humanChoice === "rock" && computerChoice === "scissors") { console.log ("Rock beats scissors! Player wins!")
+        } else if (humanChoice === "paper" && computerChoice === "rock") { 
+            console.log("Paper beats rock! Player wins!");  humanScore++; return(humanScore);
         
-    } else if (humanChoice === "paper" && computerChoice === "scissors") { console.log ("Scissors beats paper! Computer wins!")
+        }   
+    }
 
-    } else if (humanChoice === "paper" && computerChoice === "rock") {console.log("Paper beats rock! Player wins!")
-    
-    }   
-}
-
+let humanScore = 0;
+let computerScore = 0;    
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
-let humanScore = 0;
-let computerScore = 0;
 playRound(humanSelection, computerSelection);
