48d914869d6f37291b03035415aab58128349b5c
Fita04 <crackers0404@gmail.com>
2026-02-24 12:32:51 +0800

    Added the function playGame, five instances of playRound, and a console.log message that displays the current score Added the function playGame which nests EVERYTHING that makes the game run inside it. Meaning all functions will run only when playGame is called. Each function has no problem acquiring the variables it needs.
    Added five calls for playRound inside playGame so that every game needs five rounds before it's over.
    Also added a console.log message that displays the current score that is placed immediately after the each instance of playRound.
    
    Added an if-else block as well to display the result of the game after all five rounds are over.

--- script.js
+++ script.js
@@ -1,61 +1,73 @@
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+function playGame (){ //put everything inside the playGame function so that the game only starts when the function is called
+    function getComputerChoice () { // Create a function that returns the computer's choice of rock, paper, or scissors
 
-function getComputerChoice () { // Create a function that returns the computer's choice of rock, paper, or scissors
+        let computerChoice = Math.random(); // Assign each possible move a numerical value. Use Math.random() for this
 
-    let computerChoice = Math.random(); // Assign a numerical value for each possible move. Use Math.random() for this
+        if (computerChoice < 0.34) { return("rock");
+            
+        } else if (computerChoice > 0.34 && computerChoice < 0.67) { return("paper");
 
-    if (computerChoice < 0.34) { return("rock");
-        
-    } else if (computerChoice > 0.34 && computerChoice < 0.67) { return("paper");
+        } else { return ("scissors");
 
-    } else { return ("scissors");
-
-    }
-}
-
-function getHumanChoice () { // Create a function that returns the player's choice by using a prompt
-    let humanChoice = prompt("What's your move?");
-
-    if (humanChoice.toLowerCase() === "rock") { return("rock"); // the .toLowerCase method was added so that any variation of the moves are valid, provided they're spelled correctly
-        
-    } else if (humanChoice.toLowerCase() === "paper") { return("paper")
-        
-    } else if (humanChoice.toLowerCase() === "scissors") { return("scissors") 
-
+        }
     }
 
-}
+    function getHumanChoice () { // Create a function that returns the player's choice by using a prompt
+        let humanChoice = prompt("What's your move?");
 
-function playGame (){
-    function playRound (humanChoice, computerChoice) {
-        if (humanChoice === computerChoice) { console.log("Draw!");
+        if (humanChoice.toLowerCase() === "rock") { return("rock"); // the .toLowerCase method was added so that any variation of the moves are valid, provided they're spelled correctly
             
-        } else if (humanChoice === "scissors" && computerChoice === "rock" ) { 
-            console.log("Rock beats scissors! Computer wins!"); computerScore++; return(computerScore);  // computerScore++ increments the value of the variable by 1. Return sends it to the global scope so that it's value can be increased
+        } else if (humanChoice.toLowerCase() === "paper") { return("paper")
+            
+        } else if (humanChoice.toLowerCase() === "scissors") { return("scissors") 
 
-        } else if (humanChoice === "scissors" && computerChoice === "paper" ) { 
-            console.log("Scissors beats paper! Player wins!"); humanScore++; return(humanScore);  
-            
-        } else if (humanChoice === "rock" && computerChoice === "paper") { 
-            console.log("Paper beats rock! Computer wins!"); computerScore++; return(computerScore);  
-        
-        } else if (humanChoice === "rock" && computerChoice === "scissors") { 
-            console.log ("Rock beats scissors! Player wins!"); humanScore++; return(humanScore);
-            
-        } else if (humanChoice === "paper" && computerChoice === "scissors") { 
-            console.log ("Scissors beats paper! Computer wins!"); computerScore++; return(computerScore); 
+        }
 
-        } else if (humanChoice === "paper" && computerChoice === "rock") { 
-            console.log("Paper beats rock! Player wins!");  humanScore++; return(humanScore);
-        
-        }   
+    }
+        function playRound (humanChoice, computerChoice) { //use the returned string values from the getChoices as parameters to customize the result of each round
+            if (humanChoice === computerChoice) { console.log("Draw!");
+                
+            } else if (humanChoice === "scissors" && computerChoice === "rock" ) { 
+                console.log("Rock beats scissors! Computer wins!"); computerScore++; return(computerScore);  // computerScore++ increments the value of the variable by 1. Return sends it to the global scope so that it's value can be increased
+
+            } else if (humanChoice === "scissors" && computerChoice === "paper" ) { 
+                console.log("Scissors beats paper! Player wins!"); humanScore++; return(humanScore);  
+                
+            } else if (humanChoice === "rock" && computerChoice === "paper") { 
+                console.log("Paper beats rock! Computer wins!"); computerScore++; return(computerScore);  
+            
+            } else if (humanChoice === "rock" && computerChoice === "scissors") { 
+                console.log ("Rock beats scissors! Player wins!"); humanScore++; return(humanScore);
+                
+            } else if (humanChoice === "paper" && computerChoice === "scissors") { 
+                console.log ("Scissors beats paper! Computer wins!"); computerScore++; return(computerScore); 
+
+            } else if (humanChoice === "paper" && computerChoice === "rock") { 
+                console.log("Paper beats rock! Player wins!");  humanScore++; return(humanScore);
+            
+            }   
+        }
+
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        let humanScore = 0; 
+        let computerScore = 0;  
+          
+        playRound(humanSelection, computerSelection); console.log("Player:", humanScore,"Computer:", computerScore) //the console.log message displays the current score  
+        playRound(getHumanChoice(), getComputerChoice()); console.log("Player:", humanScore,"Computer:", computerScore) //put the getChoice functions as parameters so that you get a new value each time the function is called  
+        playRound(getHumanChoice(), getComputerChoice()); console.log("Player:", humanScore,"Computer:", computerScore)   
+        playRound(getHumanChoice(), getComputerChoice()); console.log("Player:", humanScore,"Computer:", computerScore)   
+        playRound(getHumanChoice(), getComputerChoice()); console.log("Player:", humanScore,"Computer:", computerScore)   
+
+            if (humanScore>computerScore) { console.log("Player wins!")  
+                
+            } else if (humanScore === computerScore) { console.log("It's a draw!")
+                
+            }
+            
+            else { console.log("Computer wins!")
+                
+            }
     }
 
-    let humanScore = 0;
-    let computerScore = 0;    
-    playRound(humanSelection, computerSelection);
-}
-
-playGame(1);
-playGame(2);
+playGame()
