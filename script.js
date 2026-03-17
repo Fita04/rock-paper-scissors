bca4fb01bc729fefcd8eafd7af489a0cd18209ef
Fita04 <crackers0404@gmail.com>
2026-03-17 17:12:06 +0800

    Added the logic to determine the winner after 5 rounds and changed some function names Added the function checkGameWinner and put it inside the button function. checkGameWinner checks the winner after five rounds are played and resets scores and round instances back to 0 after a game. Changed playRound to checkRoundWinner. Does mostly the same thing Will work on the UI section next since the logic is finished

--- script.js
+++ script.js
@@ -7,75 +7,83 @@ resultContainer.appendChild(results);
 
 let humanScore = 0; 
 let computerScore = 0;  
+let roundInstance = 0;
 
 let humanSelection; 
+
+let currentRoundResult;
 
 
 let computerSelection = function () { 
     let computerChoice = Math.random(); 
 
-    if (computerChoice < 0.34) { return("rock");
+    if (computerChoice <= 0.34) { return("rock");
         
-    } else if (computerChoice > 0.34 && computerChoice < 0.67) { return("paper");
+    } else if (computerChoice > 0.34 && computerChoice <= 0.67) { return("paper");
 
     } else { return ("scissors");
 
     }
 }
 
-buttons.forEach((button) => {
-    button.addEventListener("click", () => {;
-        humanSelection = button.value;
-        computerSelection();
-        console.log(button.value);
-        console.log(computerSelection());
-        console.log(playRound(humanSelection, computerSelection));
-    });
-});
 
-function playRound (humanSelection, computerSelection) { 
+function checkRoundWinner (human, computer) { 
     let resultOfRound = document.createElement("li");
 
-    if (humanSelection === computerSelection) { console.log("Draw!");
+    if (human === computer) { console.log("Draw!");
         
-    } else if (humanSelection === "scissors" && computerSelection === "rock" ) { 
+    } else if (human === "scissors" && computer === "rock" ) { 
         console.log("Rock beats scissors! Computer wins!"); computerScore++; return(computerScore); 
-    } else if (humanSelection === "scissors" && computerSelection === "paper" ) { 
+    } else if (human === "scissors" && computer === "paper" ) { 
         console.log("Scissors beats paper! Player wins!"); humanScore++; return(humanScore);  
         
-    } else if (humanSelection === "rock" && computerSelection === "paper") { 
+    } else if (human === "rock" && computer === "paper") { 
         console.log("Paper beats rock! Computer wins!"); computerScore++; return(computerScore);  
     
-    } else if (humanSelection === "rock" && computerSelection === "scissors") { 
+    } else if (human === "rock" && computer === "scissors") { 
         console.log ("Rock beats scissors! Player wins!"); humanScore++; return(humanScore);
         
-    } else if (humanSelection === "paper" && computerSelection === "scissors") { 
+    } else if (human === "paper" && computer === "scissors") { 
         console.log ("Scissors beats paper! Computer wins!"); computerScore++; return(computerScore); 
 
-    } else if (humanSelection === "paper" && computerSelection === "rock") { 
+    } else if (human === "paper" && computer === "rock") { 
         console.log("Paper beats rock! Player wins!");  humanScore++; return(humanScore);
     
-    }   
+    }
+
 }
 
-function playGame (){   
-
-
-    let roundInstance = 0;
-   
-    do { playRound();
-        
-    } while (roundInstance < 5);
-
-    roundInstance++; 
-
-    if (roundInstance > 5 && humanScore>computerScore) { console.log("Player wins!");  
-        
-    } else if ( roundInstance > 5 && humanScore === computerScore) { console.log("It's a draw!");
-        
-    }
+function checkGameWinner () { 
     
-    else if (roundInstance > 5 && computerScore>humanScore) { console.log("Computer wins!");
+    if (roundInstance === 5)
+        {if (humanScore > computerScore) { console.log("Player wins the game!"); roundInstance = 0; humanScore = 0; computerScore = 0;
         
+        } else if (computerScore > humanScore) { console.log("Computer wins!"); roundInstance = 0; humanScore = 0; computerScore = 0;
+        
+        } else { console.log("It's a draw!"); roundInstance = 0; humanScore = 0; computerScore = 0;
+        
+        }
     }
 }
+
+buttons.forEach((button) => {
+    button.addEventListener("click", () => {;
+
+        roundInstance++;
+
+        humanSelection = button.value;
+        let currentCompSelection = computerSelection();
+
+        console.log(button.value);
+        console.log(currentCompSelection);
+       
+        checkRoundWinner(humanSelection, currentCompSelection);
+
+        console.log(roundInstance)
+
+        checkGameWinner();
+
+
+        return roundInstance;
+    });
+});
